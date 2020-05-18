6fd542095a5ea3bda3a6a47302402d8ea2f28b79
Eli Magallanes <elimaga1996@gmail.com>
2020-05-18 07:20:15 -0700

    F - utilized store in product list component to save state of showProductCode value, even when navigating away from component

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,9 +1,6 @@
 export function reducer(state, action) {
     switch (action.type) {
-        case 'TOGGLE_PRODUCT_CODE':
-            console.log('existing state', JSON.stringify(state));
-            console.log('action payload', action.payload);
-            
+        case 'TOGGLE_PRODUCT_CODE':            
             return {
                 ...state,
                 showProductCode: action.payload
