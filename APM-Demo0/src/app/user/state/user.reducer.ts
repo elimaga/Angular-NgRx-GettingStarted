caa5e7c9274cf859b4c87e3f5c0124ecf4026fef
Eli Magallanes <elimaga1996@gmail.com>
2020-05-21 07:21:43 -0700

    F - created UserState type and gave type in reducer

--- APM-Demo0/src/app/user/state/user.reducer.ts
+++ APM-Demo0/src/app/user/state/user.reducer.ts
@@ -1,8 +1,11 @@
+import { User } from '../user';
+
 export interface UserState {
     maskUserName: boolean;
+    currentUser: User;
 }
 
-export function reducer(state, action) {
+export function reducer(state: UserState, action) {
     switch(action.type) {
         case 'TOGGLE_USER_NAME_MASK':
             return {
