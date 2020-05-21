097a49d4806912005e1aab55134d9f71b1c6070e
Eli Magallanes <elimaga1996@gmail.com>
2020-05-21 07:20:16 -0700

    a- formatting

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -3,47 +3,47 @@ import * as fromRoot from '../../state/app.state';
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 export interface State extends fromRoot.State {
-    products: ProductState;
+  products: ProductState;
 }
 
 export interface ProductState {
-    showProductCode: boolean;
-    currentProduct: Product;
-    products: Product[];
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
 }
 
 const initialState: ProductState = {
-    showProductCode: true,
-    currentProduct: null,
-    products: []
+  showProductCode: true,
+  currentProduct: null,
+  products: []
 }
 
 const getProductFeatureState = createFeatureSelector<ProductState>('products');
 
 export const getShowProductCode = createSelector(
-    getProductFeatureState, 
-    state => state.showProductCode
+  getProductFeatureState,
+  state => state.showProductCode
 )
 
 export const getCurrentProduct = createSelector(
-    getProductFeatureState,
-    state => state.currentProduct
+  getProductFeatureState,
+  state => state.currentProduct
 )
 
 export const getProducts = createSelector(
-    getProductFeatureState,
-    state => state.products
+  getProductFeatureState,
+  state => state.products
 )
 
 export function reducer(state = initialState, action): ProductState {
-    switch (action.type) {
-        case 'TOGGLE_PRODUCT_CODE':            
-            return {
-                ...state,
-                showProductCode: action.payload
-            };
+  switch (action.type) {
+    case 'TOGGLE_PRODUCT_CODE':
+      return {
+        ...state,
+        showProductCode: action.payload
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
