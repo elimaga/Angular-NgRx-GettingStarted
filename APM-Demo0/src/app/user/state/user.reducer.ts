2f899f21061399280d1b43dabf5c8f844a3b4f4f
Eli Magallanes <elimaga1996@gmail.com>
2020-05-21 07:23:27 -0700

    F - Created initial UserState and assigned as default for reducer;

--- APM-Demo0/src/app/user/state/user.reducer.ts
+++ APM-Demo0/src/app/user/state/user.reducer.ts
@@ -1,19 +1,24 @@
 import { User } from '../user';
 
 export interface UserState {
-    maskUserName: boolean;
-    currentUser: User;
+  maskUserName: boolean;
+  currentUser: User;
 }
 
-export function reducer(state: UserState, action) {
-    switch(action.type) {
-        case 'TOGGLE_USER_NAME_MASK':
-            return {
-                ...state,
-                maskUserName: action.payload
-            }
-        
-        default:
-            return state;
-    }
+const initialState: UserState = {
+  maskUserName: false,
+  currentUser: null
+}
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'TOGGLE_USER_NAME_MASK':
+      return {
+        ...state,
+        maskUserName: action.payload
+      }
+
+    default:
+      return state;
+  }
 }
