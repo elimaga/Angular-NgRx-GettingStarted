422c9f29a8e718fc013ce328f09a08ed053287b7
Eli Magallanes <elimaga1996@gmail.com>
2020-05-25 21:05:15 -0700

    R!! - refactored to use the store instead of a behavior subject in the service to keep track of the current product

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
-
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+
+/* NgRx */
 import { Store, select } from '@ngrx/store';
 import * as fromProduct from '../state/product.reducer';
 import * as productActions from '../state/product.actions';
